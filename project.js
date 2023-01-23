ace7dea4ac7f5ab6787a43b7a735f919605a880c
Chikodi Merenu <cmerenu@gmail.com>
2023-01-23 14:59:49 -0600

    added deck

---
+++ project.js
@@ -0,0 +1,41 @@
+////////////////////////////////
+// Global Variables Here
+
+let suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
+let cardValues = [
+  'A',
+  'K',
+  'Q',
+  'J',
+  '10',
+  '9',
+  '8',
+  '7',
+  '6',
+  '5',
+  '4',
+  '3',
+  '2'
+]
+let playerFirstCard = 10
+let playerSecondCard = 5
+let playerCards = []
+let dealerCards = []
+const card = document.querySelectorAll('.card')
+////////////////////////////////
+// Functions For Game Logic Here
+function addPlayerCards() = {
+  for(let i = 0;i<cardValues.length;i++) {
+let cardSum = playerFirstCard + playerSecondCard
+  } return cardSum
+
+}
+function startGame() = {
+  for ()
+
+}
+const restartGame() = {
+
+}
+////////////////////////////////
+// Event Listeners Here
