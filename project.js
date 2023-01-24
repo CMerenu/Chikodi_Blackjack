644cd68baf56cc13b89d57df3ca71fc4d40cb337
Chikodi Merenu <cmerenu@gmail.com>
2023-01-24 09:38:14 -0600

    shuffle function and deal function

--- project.js
+++ project.js
@@ -1,41 +1,195 @@
 ////////////////////////////////
 // Global Variables Here
-
-let suits = ['Clubs', 'Hearts', 'Diamonds', 'Spades']
-let cardValues = [
-  'A',
-  'K',
-  'Q',
-  'J',
-  '10',
-  '9',
-  '8',
-  '7',
-  '6',
-  '5',
-  '4',
-  '3',
-  '2'
-]
-let playerFirstCard = 10
-let playerSecondCard = 5
-let playerCards = []
-let dealerCards = []
+let deck = {
+  aceDiamonds: {
+    value: 1
+  },
+  twoDiamonds: {
+    value: 2
+  },
+  threeDiamonds: {
+    value: 3
+  },
+  fourDiamonds: {
+    value: 4
+  },
+  fiveDiamonds: {
+    value: 5
+  },
+  sixDiamonds: {
+    value: 6
+  },
+  sevenDiamonds: {
+    value: 7
+  },
+  eightDiamonds: {
+    value: 8
+  },
+  nineDiamonds: {
+    value: 9
+  },
+  tenDiamonds: {
+    value: 10
+  },
+  jackDiamonds: {
+    value: 10
+  },
+  queenDiamonds: {
+    value: 10
+  },
+  kingDiamonds: {
+    value: 10
+  },
+  aceSpades: {
+    value: 1
+  },
+  twoSpades: {
+    value: 2
+  },
+  threeSpades: {
+    value: 3
+  },
+  fourSpades: {
+    value: 4
+  },
+  fiveSpades: {
+    value: 5
+  },
+  sixSpades: {
+    value: 6
+  },
+  sevenSpades: {
+    value: 7
+  },
+  eightSpades: {
+    value: 8
+  },
+  nineSpades: {
+    value: 9
+  },
+  tenSpades: {
+    value: 10
+  },
+  jackSpades: {
+    value: 10
+  },
+  queenSpades: {
+    value: 10
+  },
+  kingSpades: {
+    value: 10
+  },
+  aceHearts: {
+    value: 1
+  },
+  twoHearts: {
+    value: 2
+  },
+  threeHearts: {
+    value: 3
+  },
+  fourHearts: {
+    value: 4
+  },
+  fiveHearts: {
+    value: 5
+  },
+  sixHearts: {
+    value: 6
+  },
+  sevenHearts: {
+    value: 7
+  },
+  eightHearts: {
+    value: 8
+  },
+  nineHearts: {
+    value: 9
+  },
+  tenHearts: {
+    value: 10
+  },
+  jackHearts: {
+    value: 10
+  },
+  queenHearts: {
+    value: 10
+  },
+  kingHearts: {
+    value: 10
+  },
+  aceClubs: {
+    value: 1
+  },
+  twoClubs: {
+    value: 2
+  },
+  threeClubs: {
+    value: 3
+  },
+  fourClubs: {
+    value: 4
+  },
+  fiveClubs: {
+    value: 5
+  },
+  sixClubs: {
+    value: 6
+  },
+  sevenClubs: {
+    value: 7
+  },
+  eightClubs: {
+    value: 8
+  },
+  nineClubs: {
+    value: 9
+  },
+  tenClubs: {
+    value: 10
+  },
+  jackClubs: {
+    value: 10
+  },
+  queenClubs: {
+    value: 10
+  },
+  kingClubs: {
+    value: 10
+  }
+}
+console.log(deck)
+// let playerFirstCard = deck[i]
+// let playerSecondCard = deck[i]
+let playerCards = {}
+let dealerCards = {}
 const card = document.querySelectorAll('.card')
 ////////////////////////////////
 // Functions For Game Logic Here
-function addPlayerCards() = {
-  for(let i = 0;i<cardValues.length;i++) {
-let cardSum = playerFirstCard + playerSecondCard
-  } return cardSum
-
+function shuffleCards() {
+  deck.sort((a, b) => 0.5 - Math.random())
 }
-function startGame() = {
-  for ()
-
+function dealCards() {
+  let x = deck[0]
+  playerCards.push(x)
+  deck.shift()
+  playerCards.push(x)
+  deck.shift()
+  dealerCards.push(x)
+  deck.shift()
+  dealerCards.push(x)
+  deck.shift()
 }
-const restartGame() = {
+console.log(playerCards)
+console.log(dealerCards)
 
+function sumPlayerCards() {
+  for (let i = 0; i < cardValues.length; i++) {
+    let cardSum = playerCards[0].value + playerCards[1].value
+  }
+  return cardSum
 }
-////////////////////////////////
+// const restartGame(){
+// }
+// ////////////////////////////////
 // Event Listeners Here
